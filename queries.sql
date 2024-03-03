6800fa7c9bdd38638cbda8ddfde0bb08cbe4b1a0
g1ammi93 <giammariacarelli93@hotmail.it>
2024-03-03 16:33:32 +0000

    Added queries 5

--- queries.sql
+++ queries.sql
@@ -5,13 +5,13 @@ SELECT * FROM `students` WHERE `date_of_birth` LIKE '1990%';
 SELECT * FROM `courses` WHERE `cfu` > 10; 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT * FROM `students` WHERE YEAR(`date_of_birth`) <= 1994; 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = '1'; 
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-
+SELECT * FROM `exams` WHERE `hour` > '14:00:00' AND date = '2020-06-20'; 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
