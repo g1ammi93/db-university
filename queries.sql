206b9039f8e04265f0cd07da4c9f6f830a49c8b2
g1ammi93 <giammariacarelli93@hotmail.it>
2024-03-03 16:37:30 +0000

    Added querie 6

--- queries.sql
+++ queries.sql
@@ -14,7 +14,7 @@ SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = '1';
 SELECT * FROM `exams` WHERE `hour` > '14:00:00' AND date = '2020-06-20'; 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
-
+SELECT * FROM `degrees` WHERE `level` = 'magistrale'; 
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
