a22fa72a6f1c8bfff70f9361b0082177a32dd394
g1ammi93 <giammariacarelli93@hotmail.it>
2024-03-03 16:39:15 +0000

    Added querie 7

--- queries.sql
+++ queries.sql
@@ -17,6 +17,6 @@ SELECT * FROM `exams` WHERE `hour` > '14:00:00' AND date = '2020-06-20';
 SELECT * FROM `degrees` WHERE `level` = 'magistrale'; 
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(*) FROM `departments`; 
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
