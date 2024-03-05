7c89e2c7b8470c63154636ed4af94622ef37b4a8
g1ammi93 <giammariacarelli93@hotmail.it>
2024-03-05 09:03:48 +0000

    Added Queries Join

--- queries.sql
+++ queries.sql
@@ -40,3 +40,28 @@ SELECT `exam_id` , AVG(`vote`) FROM `exam_student` GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(*), `degree_id` FROM `courses` GROUP BY `degree_id`; 
+
+
+
+-- JOIN
+
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT S.`name`, `S`.`surname` FROM `students` AS S JOIN `degrees` AS D ON `S`.`degree_id` = D.`id` WHERE D.`name` = 'Corso di Laurea in Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT * FROM `degrees` AS DEG JOIN `departments` AS DEP ON `DEG`.`department_id` = DEP.`id` WHERE DEP.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * FROM `courses` AS C JOIN `course_teacher` AS CT ON `CT`.`course_id` = C.`id` JOIN `teachers` AS T ON T.`id` = `CT`.`teacher_id` WHERE T.`name` = 'Fulvio' AND `T`.`surname` = 'Amato';
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `S`.`name`, `S`.`surname`,`D`.`name` AS 'Corso Laurea', `departments`.`name` AS 'Dipartimento' FROM `students` AS S JOIN `degrees` AS D ON `S`.`degree_id` = `D`.`id` JOIN `departments` ON `D`.`department_id` = `departments`.`id` ORDER BY `S`.`surname`, `S`.`name`;
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
